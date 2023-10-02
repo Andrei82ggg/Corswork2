041e4a6bb17ca183b5b271e1c767e00b708415ea
Андрей <ustu1982@mail.ru>
2023-10-02 09:43:40 +0500

    штв

---
+++ src/main/java/pro/sky/ind11cw2/Ind11Cw2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.ind11cw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ind11Cw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ind11Cw2Application.class, args);
+	}
+
+}
